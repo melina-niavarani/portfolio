47f064cf59115a8204e3a7321385ccb36f714485
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-05-25 16:42:15 +0330

    built components for each part

---
+++ portfolio/src/components/footer/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
+        <p>Coded in <!-- --> 
+        <a href="https://code.visualstudio.com/" class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300" target="_blank" rel="noreferrer">Visual Studio Code</a> <!-- --> by Melina Niavarani. Built with <!-- --> 
+        <a href="https://vuejs.org/" class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300" target="_blank" rel="noreferrer"> Vue.js</a> <!-- --> and <!-- -->
+        <a href="https://tailwindcss.com/" class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300" target="_blank" rel="noreferrer">Tailwind CSS.</a>
+        All text is set in the <!-- -->
+        <a href="https://rsms.me/inter/" class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300" target="_blank" rel="noreferrer">Inter</a> <!-- --> typeface.
+        </p>
+    </footer>
+</template>
