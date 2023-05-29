711ed8da7232b114f75208514f65f4741250f84c
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-05-29 11:49:56 +0330

    change the first project name

--- portfolio/src/components/projects/archive/ArchiveTable.vue
+++ portfolio/src/components/projects/archive/ArchiveTable.vue
@@ -17,9 +17,17 @@
                 <td class="py-4 pr-4 align-top font-semibold leading-snug text-slate-200">
                     <div>
                         <div class="block sm:hidden">
-                            <span>My personal Portfolio</span>
+                            <a class="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 hover:text-slate-200 focus-visible:text-teal-300 sm:hidden group/link text-base" href="https://melinaniavarani.com" target="_blank" rel="noreferrer" aria-label="Pratt">
+                                <span>
+                                    <span class="inline-block">Portfolio
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true">
+                                            <path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path>
+                                        </svg>
+                                    </span>
+                                </span>
+                            </a>
                         </div>
-                        <div class="hidden sm:block">My personal Portfolio</div>
+                        <div class="hidden sm:block">IAMDB</div>
                     </div>
                 </td>
                 <td class="hidden py-4 pr-4 align-top text-sm lg:table-cell">
