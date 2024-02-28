d2b1404c8c65ea5b00ac8dfc8c904f6edf77d4a3
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2024-02-28 15:37:41 +0330

    Create Archive.vue

---
+++ src/views/Archive.vue
@@ -0,0 +1,32 @@
+<template>
+    <div @mousemove="handleMouseMove" class="__variable_20b187 group/spotlight relative">
+        <div class="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute" 
+            :style="{background: `radial-gradient(600px at ${xPosition}px ${yPosition}px, rgba(29, 78, 216, 0.15), transparent 80%)`}">
+        </div>
+        <projectArchive/>
+    </div>
+</template>
+
+<script>
+import projectArchive from '@/components/projects/archive/ArchiveDeatails.vue'
+
+export default {
+components: {
+    projectArchive,
+},
+  data() {
+    return {
+      xPosition: `519`,
+      yPosition: `322`,
+    }
+  },
+  methods: {
+     handleMouseMove({x , y}) {
+      this.xPosition = x
+      this.yPosition = y
+      console.log( this.xPosition)
+     }
+  }
+
+}
+</script>
